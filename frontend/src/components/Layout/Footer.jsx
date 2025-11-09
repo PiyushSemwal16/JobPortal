dbef8f93a494e72f80a4d1d5ea280b956a92210e
PiyushSemwal16 <dattpiyush68@gmail.com>
2025-11-09 12:32:18 +0530

    First Commit

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -2,18 +2,18 @@ import React, { useContext } from 'react'
 import {Context} from "../../main"
 import {Link} from "react-router-dom"
 import { FaGithub , FaLinkedin} from "react-icons/fa"
-import { SiLeetcode } from "react-icons/si";
-import { RiInstagramFill} from "react-icons/ri"
+// import { SiLeetcode } from "react-icons/si";
+// import { RiInstagramFill} from "react-icons/ri"
 function Footer() {
   const {isAuthorized}  = useContext(Context)
   return (
     <footer className= {isAuthorized ? "footerShow" : "footerHide"}>
-<div>&copy; All Rights Reserved by Abhishek.</div>
+<div>&copy; All Rights Reserved by Piyush.</div>
 <div>
-  <Link to={'https://github.com/exclusiveabhi'} target='github'><FaGithub></FaGithub></Link>
-  <Link to={'https://leetcode.com/u/exclusiveabhi/'} target='leetcode'><SiLeetcode></SiLeetcode></Link>
-  <Link to={'https://www.linkedin.com/in/abhishek-rajput-/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
-  <Link to={'https://www.instagram.com/exclusiveabhi/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link>
+  <Link to={'https://github.com/piyushsemwal16'} target='github'><FaGithub></FaGithub></Link>
+  {/* <Link to={'https://leetcode.com/u/'} target='leetcode'><SiLeetcode></SiLeetcode></Link> */}
+  <Link to={'https://www.linkedin.com/in/piyush-semwal-52b565261/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
+  {/* <Link to={'https://www.instagram.com/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link> */}
 </div>
       
     </footer>
